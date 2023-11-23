f22e3b73e30a4d1e6a074985a5104470a0a743b1
김지호 <jhkim022@lgupluspartners.co.kr>
2023-11-23 11:06:42 +0900

    react-player 수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import AddForm from "./components/AddForm";
 import List from "./components/List";
 import Favorite from "./components/Favorite";
 import Player from "./components/Player";
-import { trackUpdate } from "./reducer/reducer";
+// import { trackUpdate } from "./reducer/reducer";
 
 function App() {
   const dispatch = useDispatch();
@@ -16,20 +16,6 @@ function App() {
 
   const list = useSelector((state) => state.playlist);
   // 플레이리스트 state list
-
-  // 트랙 업데이트 함수 redux로 action 시킴
-  useEffect(() => {
-    if (list.length !== 0) {
-      let arr = [];
-      list.forEach((value, index) => {
-        return arr.push(list[index].url);
-      });
-      dispatch(trackUpdate(arr));
-    }
-  }, [list]);
-
-  // 트랙 업데이트 함수 redux로 action 시킴
-  // recomend_data의 비공개 동영상은 재생이 안됨
 
   return (
     <div className="App">
